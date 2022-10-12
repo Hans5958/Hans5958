a8b54bc410a903a043872a11c2cb7be7925329b2
Hans5958 <hans5958@outlook.com>
2022-10-12 23:44:51 +0700

    Update dependencies, Dependabot for Actions, use @actions/core

--- src/build.js
+++ src/build.js
@@ -2,11 +2,12 @@ const print = require("./print")
 const fs = require("fs")
 const dayjs = require("dayjs")
 dayjs.extend(require('dayjs/plugin/utc'))
+const core = require('@actions/core')
 
 print()
 	.then(result => {
 		const { readme, timestamp } = result
-		console.log("::set-output name=TIMESTAMP::" + dayjs(timestamp).utc().format())
+		core.setOutput('TIMESTAMP', dayjs(timestamp).utc().format())
 		fs.writeFileSync("README.md", readme)
 	}).catch(error => {
 		console.error(error)
