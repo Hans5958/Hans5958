c6bebf6ed3b2d3768cc7757da09f5f726de78d58
Hans5958 <hans5958@outlook.com>
2025-12-11 16:55:21 +0000

    Fix commit info missing from event 2

--- src/modules/events.js
+++ src/modules/events.js
@@ -89,13 +89,12 @@ module.exports = async () => {
 				break;
 			case "PushEvent":
 				const branch = payload.ref.slice(11)
-				console.log(payload.head)
 				const commit = (await octokit.git.getCommit({
 					'owner': repo.name.split('/')[0],
 					'repo': repo.name.split('/')[1],
 					'commit_sha': payload.head
 				})).data
-				if (commit.author.name !== "Hans5958") {
+				if (commit.author.name === "Hans5958") {
 					const hash = payload.head
 					commits.push(`[\`${hash.substr(0, 7)}\`](${repoLink}/commit/${hash}) ${commit.message.split("\n")[0]} (${repoLinkMarkdown}, [${branch}](${repoLink}/tree/${branch}))`)
 				}
